4be8930a438c0a66cc56a898b562a91ad5c2eec9
Unknown <chaowa41@gmail.com>
2018-12-03 14:39:41 +0700

    3-12-61-14:41

---
+++ VB1/frmSecond.Designer.vb
@@ -0,0 +1,195 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmSecond
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtVistra = New System.Windows.Forms.TextBox()
+        Me.txtFristra = New System.Windows.Forms.TextBox()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.lblComFristra = New System.Windows.Forms.Label()
+        Me.lblComVistra = New System.Windows.Forms.Label()
+        Me.lblSumCom = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(80, 69)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(194, 33)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Vistra Gluta"
+        '
+        'Label2
+        '
+        Me.Label2.Location = New System.Drawing.Point(80, 111)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(133, 23)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Fristra Gluta"
+        '
+        'Label3
+        '
+        Me.Label3.Location = New System.Drawing.Point(80, 154)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(133, 23)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "ยอดขายทั้งหมด"
+        '
+        'Label4
+        '
+        Me.Label4.Location = New System.Drawing.Point(337, 72)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(133, 23)
+        Me.Label4.TabIndex = 0
+        Me.Label4.Text = "ค่าคอมมิชชั่น 5%"
+        '
+        'Label5
+        '
+        Me.Label5.Location = New System.Drawing.Point(337, 114)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(133, 23)
+        Me.Label5.TabIndex = 0
+        Me.Label5.Text = "ค่าคอมมิชชั่น 10%"
+        '
+        'Label6
+        '
+        Me.Label6.Location = New System.Drawing.Point(337, 154)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(133, 23)
+        Me.Label6.TabIndex = 0
+        Me.Label6.Text = "ค่าคอมมิชชั่นทั้งหมด"
+        '
+        'txtVistra
+        '
+        Me.txtVistra.Location = New System.Drawing.Point(186, 69)
+        Me.txtVistra.Name = "txtVistra"
+        Me.txtVistra.Size = New System.Drawing.Size(100, 20)
+        Me.txtVistra.TabIndex = 0
+        '
+        'txtFristra
+        '
+        Me.txtFristra.Location = New System.Drawing.Point(186, 104)
+        Me.txtFristra.Name = "txtFristra"
+        Me.txtFristra.Size = New System.Drawing.Size(100, 20)
+        Me.txtFristra.TabIndex = 1
+        '
+        'lblResult
+        '
+        Me.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblResult.Location = New System.Drawing.Point(183, 154)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(103, 23)
+        Me.lblResult.TabIndex = 0
+        '
+        'lblComFristra
+        '
+        Me.lblComFristra.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblComFristra.Location = New System.Drawing.Point(451, 111)
+        Me.lblComFristra.Name = "lblComFristra"
+        Me.lblComFristra.Size = New System.Drawing.Size(94, 23)
+        Me.lblComFristra.TabIndex = 0
+        '
+        'lblComVistra
+        '
+        Me.lblComVistra.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblComVistra.Location = New System.Drawing.Point(451, 72)
+        Me.lblComVistra.Name = "lblComVistra"
+        Me.lblComVistra.Size = New System.Drawing.Size(94, 23)
+        Me.lblComVistra.TabIndex = 0
+        '
+        'lblSumCom
+        '
+        Me.lblSumCom.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblSumCom.Location = New System.Drawing.Point(451, 154)
+        Me.lblSumCom.Name = "lblSumCom"
+        Me.lblSumCom.Size = New System.Drawing.Size(94, 23)
+        Me.lblSumCom.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.ForestGreen
+        Me.Button1.Location = New System.Drawing.Point(211, 235)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 37)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "คำนวณ"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.Green
+        Me.Button2.Location = New System.Drawing.Point(340, 235)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 37)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "ปิด"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'frmSecond
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(612, 301)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtFristra)
+        Me.Controls.Add(Me.txtVistra)
+        Me.Controls.Add(Me.lblComVistra)
+        Me.Controls.Add(Me.lblComFristra)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.lblSumCom)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmSecond"
+        Me.Text = "frmSecond"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents txtVistra As System.Windows.Forms.TextBox
+    Friend WithEvents txtFristra As System.Windows.Forms.TextBox
+    Friend WithEvents lblResult As System.Windows.Forms.Label
+    Friend WithEvents lblComFristra As System.Windows.Forms.Label
+    Friend WithEvents lblComVistra As System.Windows.Forms.Label
+    Friend WithEvents lblSumCom As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+End Class
---
+++ VB1/frmSecond.vb
@@ -0,0 +1,31 @@
+﻿Public Class frmSecond
+
+    
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim Vistra As Integer
+        Dim Fristra As Integer
+        Dim ComVistra As Integer
+        Dim ComFrist As Integer
+        Dim Result As Integer
+        Dim SumCom As Integer
+        Vistra = Val(txtVistra.Text)
+        Fristra = Val(txtFristra.Text)
+        Result = Vistra + Fristra
+        ComVistra = Vistra * 0.05
+        ComFrist = Fristra * 0.1
+        SumCom = ComVistra + ComFrist
+
+        lblResult.Text = Val(txtVistra.Text) + Val(txtFristra.Text)
+        lblComVistra.Text = Val(txtVistra.Text) * 0.05
+        lblComFristra.Text = Val(txtFristra.Text) * 0.1
+        lblSumCom.Text = Val(lblComVistra.Text) + Val(lblComFristra.Text)
+
+        lblResult.BackColor = Color.CadetBlue
+
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+End Class
